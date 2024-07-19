1f508e54694f09ea6db99bcf7fc98a7a1a7491f7
Shengy <shengy90@gmail.com>
2024-07-19 10:35:35 +0100

    Create LCL Data Modules

--- src/data_modules/lcl_data_module.py
+++ src/data_modules/lcl_data_module.py
@@ -21,31 +21,17 @@ class LCLData(Dataset):
     def __init__(
         self,
         data_path: Path,
-        stats_path: Path,
+        feature_mean: float,
+        feature_std: float,
         n_samples: int,
     ):
-        """
-        Args:
-            data_path (Path): Data path
-            stats_path (Path): Stats path. Note when loading
-            evaluation dataset, the stats path point to the
-            stats of training data, rather than evaluation data
-            to avoid data leakage!
-            n_samples (int): Number of samples to load
-        """
-        self.df = pd.read_csv(f"{data_path}/lcl_data.csv")
-        self.df_stats = pd.read_csv(f"{stats_path}/mean_std.csv")
-
-        # Parse stats
-        self.feature_mean = self.df_stats["mean"].values[0]
-        self.feature_std = self.df_stats["stdev"].values[0]
-
-        # Resample Dataset
+        self.df = pd.read_csv(data_path)
         self.n_samples = n_samples
         self.df = self.df.sample(
             self.n_samples, random_state=RANDOM_STATE
         ).reset_index(drop=True)
-
+        self.feature_mean = feature_mean
+        self.feature_std = feature_std
         self.kwh = self.df["kwh"].apply(ast.literal_eval)
         self.kwh = torch.from_numpy(np.array(self.kwh.tolist())).float()
         self.month = self.df["month"]
@@ -94,15 +80,15 @@ class LCLDataModule(pl.LightningDataModule):
     def __init__(
         self,
         data_path: Path,
-        stats_path: Path,
         batch_size: int,
-        n_samples: int,
+        feature_mean: float,
+        feature_std: float,
     ):
         super().__init__()
         self.data_path = data_path
-        self.stats_path = stats_path
         self.batch_size = batch_size
-        self.n_samples = n_samples
+        self.feature_mean = feature_mean
+        self.feature_std = feature_std
 
     def prepare_data(self):
         pass
@@ -110,8 +96,8 @@ class LCLDataModule(pl.LightningDataModule):
     def setup(self, stage=""):
         self.dataset = LCLData(
             data_path=self.data_path,
-            stats_path=self.stats_path,
-            n_samples=self.n_samples,
+            feature_mean=self.feature_mean,
+            feature_std=self.feature_std,
         )
 
     def train_dataloader(self):
