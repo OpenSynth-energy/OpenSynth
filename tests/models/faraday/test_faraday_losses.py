Fix bug in the way quantile and mse losses were calculating averages
Can't just use simple average - need to calculate the sum of losses and divide by the sum of weights

tests/models/faraday/test_faraday_losses.py
@@ -76,7 +76,7 @@ class TestFaradayLosses:
         [
             # Exact tensor should return quantile loss of 0
             pytest.param(tensor1, tensor1, 0.5, 0),
-            pytest.param(tensor1, tensor2, 0.5, 4.1),
+            pytest.param(tensor1, tensor2, 0.5, 1.86),
         ],
     )
     def test_quantile_loss_with_weights(
@@ -85,14 +85,44 @@ class TestFaradayLosses:
         got_loss = losses.quantile_loss(t1, t2, quantile, self.weights)
         assert torch.round(got_loss, decimals=2) == expected_value
 
+    def test_quantile_loss_weighted_and_expanded_same_values(self):
+        # Test supplying weights gives the same
+        # results as manually expanding the tensors and
+        # calculating the quantile loss
+        weighted_quantile_loss = losses.quantile_loss(
+            self.tensor1, self.tensor2, 0.5, self.weights
+        )
+        tensor1_expanded = losses._expand_samples(self.tensor1, self.weights)
+        tensor2_expanded = losses._expand_samples(self.tensor2, self.weights)
+        unweighted_quantile_loss = losses.quantile_loss(
+            tensor1_expanded, tensor2_expanded, 0.5, None
+        )
+
+        assert torch.round(
+            unweighted_quantile_loss, decimals=2
+        ) == torch.round(weighted_quantile_loss, decimals=2)
+
     @pytest.mark.parametrize(
         "t1,t2,expected_value",
         [
             # Exact tensor should return quantile loss of 0
             pytest.param(tensor1, tensor1, 0),
-            pytest.param(tensor1, tensor2, 34.6),
+            pytest.param(tensor1, tensor2, 15.73),
         ],
     )
     def test_mse_loss(self, t1, t2, expected_value):
         got_loss = losses.mse_loss(t1, t2, self.weights)
-        assert got_loss == expected_value
+        assert torch.round(got_loss, decimals=2) == expected_value
+
+    def test_mse_loss_weighted_and_expanded_same_values(self):
+        tensor1_expanded = losses._expand_samples(self.tensor1, self.weights)
+        tensor2_expanded = losses._expand_samples(self.tensor2, self.weights)
+        weighted_mse_loss = losses.mse_loss(
+            self.tensor1, self.tensor2, self.weights
+        )
+        unweighted_mse_loss = losses.mse_loss(
+            tensor1_expanded, tensor2_expanded, None
+        )
+        assert torch.round(weighted_mse_loss, decimals=2) == torch.round(
+            unweighted_mse_loss, decimals=2
+        )