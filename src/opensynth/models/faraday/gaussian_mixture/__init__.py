b1b150a90ae57eb42af2a47cff38faa773745820
ianlmgoddard <ian.goddard@centrefornetzero.org>
2024-09-23 15:19:05 +0100

    add license to __init__

--- src/opensynth/models/faraday/gaussian_mixture/__init__.py
+++ src/opensynth/models/faraday/gaussian_mixture/__init__.py
@@ -1,3 +1,6 @@
+# Copyright Contributors to the Opensynth-energy Project.
+# SPDX-License-Identifier: Apache-2.0
+
 from opensynth.models.faraday.gaussian_mixture.fit_gmm import fit_gmm
 from opensynth.models.faraday.gaussian_mixture.model import (
     GaussianMixtureModel,
