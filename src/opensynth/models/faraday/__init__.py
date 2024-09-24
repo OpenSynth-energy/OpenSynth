d937c67d1c09884655c9160d896078df6b75f5dc
Charlotte Avery <charlotte.avery@centrefornetzero.org>
2024-09-24 14:12:16 +0100

    Fix bug

--- src/opensynth/models/faraday/__init__.py
+++ src/opensynth/models/faraday/__init__.py
@@ -1,7 +1,7 @@
 # Copyright Contributors to the Opensynth-energy Project.
 # SPDX-License-Identifier: Apache-2.0
 
-from opensynth.models.faraday.model import Decoder, Encoder, FaradayModel
-from opensynth.models.faraday.vae_model import FaradayVAE
+from opensynth.models.faraday.model import FaradayModel
+from opensynth.models.faraday.vae_model import Decoder, Encoder, FaradayVAE
 
 __all__ = ["FaradayVAE", "FaradayModel", "Encoder", "Decoder"]
