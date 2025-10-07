030c9321d7733cf85102ad2bc2dc6e1fa1f58033
Charlotte Avery <143102500+charlotte-avery@users.noreply.github.com>
2025-10-07 12:14:52 +0100

    Expose reparametrisation module (#82)
    * Add ReparametrisationModule to init
    
    Signed-off-by: Charlotte Avery <charlotte.avery@centrefornetzero.org>
    
    ---------
    
    Signed-off-by: Charlotte Avery <charlotte.avery@centrefornetzero.org>

--- src/opensynth/models/faraday/__init__.py
+++ src/opensynth/models/faraday/__init__.py
@@ -2,6 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from opensynth.models.faraday.model import FaradayModel
-from opensynth.models.faraday.vae_model import Decoder, Encoder, FaradayVAE
+from opensynth.models.faraday.vae_model import (
+    Decoder,
+    Encoder,
+    FaradayVAE,
+    ReparametrisationModule,
+)
 
-__all__ = ["FaradayVAE", "FaradayModel", "Encoder", "Decoder"]
+__all__ = [
+    "FaradayVAE",
+    "FaradayModel",
+    "Encoder",
+    "Decoder",
+    "ReparametrisationModule",
+]
